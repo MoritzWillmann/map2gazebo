f90c3b89ac83f6fc9fa97aa211d692d15d0c23db
moritzadlatus <moritz.willmann@adlatus-robotics.com>
2021-02-11 16:04:32 +0100

    create launchfile, adapt package structure

--- setup.py
+++ setup.py
@@ -1,17 +1,23 @@
 from setuptools import setup, find_packages
+import os
+from glob import glob
 
 package_name = 'map2gazebo'
 
 setup(
     name=package_name,
     version='0.0.0',
-    packages=find_packages(),
+    packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
+        (os.path.join('share', package_name, 'config'), glob('config/*.yaml')),
+        (os.path.join('share', package_name, 'models', 'map'), ['models/map/model.config', 'models/map/model.sdf']),
+        (os.path.join('share', package_name, 'models', 'map', 'meshes'), ['models/map/meshes/map.stl']),
     ],
-    install_requires=['setuptools', 'rclpy', 'nav_msgs'],
+    install_requires=['setuptools'],
     zip_safe=True,
     maintainer='Moritz Willmann',
     maintainer_email='moritz.willmann@uni-ulm.de',
@@ -19,7 +25,7 @@ setup(
     license='Apache License 2.0',
     entry_points={
         'console_scripts': [
-            'src.map2gazebo:main'
+            'map2gazebo = map2gazebo.map2gazebo:main'
         ],
     },
 )
